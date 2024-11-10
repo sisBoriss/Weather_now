daeb9f0583325302d92421f7e6b7a4f38dd82b70
sisBoriss <borisova.as@soctech-it.ru>
2024-11-10 15:44:25 +0300

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser },
-  files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
-  rules: {
+  {
+    languageOptions: { globals: globals.browser },
+    files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
+    rules: {
       "prefer-const": "warn",
-      "no-constant-binary-expression": "error"
-  }
-},
+      "no-constant-binary-expression": "error",
+    },
+  },
   pluginJs.configs.recommended,
 ];
--- src/script_weather_info.js
+++ src/script_weather_info.js
@@ -1,24 +1,5 @@
-(async function ()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{
-  const token = 'f07624d2b0ab1b2677906af3f93c4cad';
+(async function () {
+  const token = "f07624d2b0ab1b2677906af3f93c4cad";
   const weatherInfoEl = document.querySelector("#response-info");
   const city = getWeatherUserLocation();
   const localweather = await getWeather(city);
@@ -27,11 +8,11 @@
   //  const formEl = document.querySelector("button-59");
 
   function showWeather(el, weatherInfo) {
-    console.log("child responce " + el.childNodes.length)
-    if (el.childNodes.length<4){
-    const section = document.createElement("section");
-    section.innerHTML = JSON.stringify(weatherInfo, null, 2);
-    el.appendChild(section);
+    console.log("child responce " + el.childNodes.length);
+    if (el.childNodes.length < 4) {
+      const section = document.createElement("section");
+      section.innerHTML = JSON.stringify(weatherInfo, null, 2);
+      el.appendChild(section);
     } else {
       const innerSection = el.querySelector("section");
       innerSection.innerHTML = JSON.stringify(weatherInfo, null, 2);
@@ -53,22 +34,30 @@
     const responseLocal = await fetch(`https://get.geojs.io/v1/ip/geo.json`);
     const data = await responseLocal.json();
     const city = data.city;
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${token}`);
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${token}`,
+    );
     const jsonData = await response.json();
     const lat = jsonData[0].lat;
     const lon = jsonData[0].lon;
-    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${token}`);
+    const result = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${token}`,
+    );
     const weatherData = await result.json();
     return showWeather(weatherInfoEl, weatherData);
   }
   async function getWeather(cityName) {
-    console.log("cityName " + cityName + " token " + token)
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${token}`);
+    console.log("cityName " + cityName + " token " + token);
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${token}`,
+    );
     const jsonData = await response.json();
-    console.log(jsonData)
+    console.log(jsonData);
     const lat = jsonData[0].lat;
     const lon = jsonData[0].lon;
-    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${token}`);
+    const result = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${token}`,
+    );
     const weatherData = await result.json();
     return weatherData;
   }
@@ -79,22 +68,22 @@
     const cityName = inputEl.value;
     inputEl.value = "";
     const parentSection = document.querySelector("#history-info");
-    if (parentSection.childNodes.length <13) {
-    const asideDiv = document.createElement("section");
-    asideDiv.setAttribute('id', "cityName");
-    asideDiv.setAttribute('value', `${cityName}`);
-    asideDiv.innerHTML = `${cityName}`;
-    parentSection.appendChild(asideDiv);
+    if (parentSection.childNodes.length < 13) {
+      const asideDiv = document.createElement("section");
+      asideDiv.setAttribute("id", "cityName");
+      asideDiv.setAttribute("value", `${cityName}`);
+      asideDiv.innerHTML = `${cityName}`;
+      parentSection.appendChild(asideDiv);
     } else {
       const firstSection = document.getElementById("cityName");
       firstSection.remove();
       const asideDiv = document.createElement("section");
-      asideDiv.setAttribute('id', "cityName");
-      asideDiv.setAttribute('value', `${cityName}`);
+      asideDiv.setAttribute("id", "cityName");
+      asideDiv.setAttribute("value", `${cityName}`);
       asideDiv.innerHTML = `${cityName}`;
       parentSection.appendChild(asideDiv);
     }
-  const weather = await getWeather(cityName);
+    const weather = await getWeather(cityName);
     showWeather(weatherInfoEl, weather);
   });
 })();
