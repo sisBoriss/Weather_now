184402fbd76072725e70f478d8bba5ebbf7a6611
sisBoriss <borisova.as@soctech-it.ru>
2024-11-17 20:39:49 +0300

    update code

--- src/script_weather_info.js
+++ src/script_weather_info.js
@@ -1,100 +1,117 @@
+
 (async function ()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 {
+  let map;
+  let coord;
   const token = 'f07624d2b0ab1b2677906af3f93c4cad';
+  const map_token = '86c38176-bc99-4e59-8c87-bd7b27ed1ea5';
+  let lat = '';
+  let lon = '';
+  let httpMap = `https://static-maps.yandex.ru/v1?ll=${lon},${lat}&lang=ru_RU&size=312,360&apikey=${map_token}`;
   const weatherInfoEl = document.querySelector("#response-info");
-  const city = getWeatherUserLocation();
-  const localweather = await getWeather(city);
-  showWeather(weatherInfoEl, localweather);
-  // �������� ��������� �� ������ ��������
-  //  const formEl = document.querySelector("button-59");
+ coord = getWeatherUserLocation();
 
+ 
+  function switchMap(coord) {
+    if (map) {
+        map.destroy(); 
+    }
+
+    ymaps.ready(function () {
+        map = new ymaps.Map('map', {
+            center: [coord[0], coord[1]],
+            zoom: 10
+        });
+    });
+}
   function showWeather(el, weatherInfo) {
-    console.log("child responce " + el.childNodes.length)
     if (el.childNodes.length<4){
     const section = document.createElement("section");
-    section.innerHTML = JSON.stringify(weatherInfo, null, 2);
+    section.innerHTML = weatherInfo;
     el.appendChild(section);
     } else {
       const innerSection = el.querySelector("section");
-      innerSection.innerHTML = JSON.stringify(weatherInfo, null, 2);
+      innerSection.innerHTML = weatherInfo;
     }
   }
-
-  /**
-   * ������� ������ ������ ������ ��
-   * https://api.openweathermap.org/data/2.5/weather?units=metric&q={{CITY_NAME}}&appid={{APP_ID}}
-   * ���
-   *  {{CITY_NAME}} ������ ���� ������� �� ��� ������
-   *  {{APP_ID}} ������ ���� ������� �� ���� ����������
-   * ������ ���������� ������ � ������� JSON
-   *
-   * ������� ������ ���������� (Promise) ������ � ����������� � ������
-   * @param {string} cityName
-   */
   async function getWeatherUserLocation() {
     const responseLocal = await fetch(`https://get.geojs.io/v1/ip/geo.json`);
     const data = await responseLocal.json();
     const city = data.city;
     const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${token}`);
     const jsonData = await response.json();
-    const lat = jsonData[0].lat;
-    const lon = jsonData[0].lon;
+     lat = jsonData[0].lat;
+     lon = jsonData[0].lon;
     const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${token}`);
     const weatherData = await result.json();
-    return showWeather(weatherInfoEl, weatherData);
+
+   let textWeather = `City : ${weatherData.name}`  + "<br />" +
+      `Temperature: ${weatherData.main['temp']} ` + "<br />" +
+      `Pressure: ${weatherData.main['pressure']}` + "<br />" +
+      `Wind speed: ${weatherData.wind['speed']}` + "<br />" +
+      `Description: ${weatherData.weather[0]['description']}`;
+     showWeather(weatherInfoEl, textWeather);
+     let coordinates = [];
+     coordinates.push(lat,lon);
+     switchMap(coordinates)
+     return coordinates
   }
   async function getWeather(cityName) {
-    console.log("cityName " + cityName + " token " + token)
     const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${token}`);
     const jsonData = await response.json();
-    console.log(jsonData)
-    const lat = jsonData[0].lat;
-    const lon = jsonData[0].lon;
+     lat = jsonData[0].lat;
+     lon = jsonData[0].lon;
     const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${token}`);
     const weatherData = await result.json();
-    return weatherData;
+    let textWeather = `City : ${weatherData.name}`  + "<br />" +
+    `Temperature: ${weatherData.main['temp']} ` + "<br />" +
+    `Pressure: ${weatherData.main['pressure']}` + "<br />" +
+    `Wind speed: ${weatherData.wind['speed']}` + "<br />" +
+    `Description: ${weatherData.weather[0]['description']}`;
+  showWeather(weatherInfoEl, textWeather);
+  let coordinates = [];
+  coordinates.push(lat,lon);
+  return coordinates
   }
-  const getWeatherb = document.getElementById("getWeatherb");
-  getWeatherb.addEventListener("click", async (ev) => {
+  document.addEventListener("DOMContentLoaded", function() {
+  getWeatherb.addEventListener("click",async  (ev) => {
     ev.preventDefault();
     const inputEl = document.querySelector("#userInput");
     const cityName = inputEl.value;
     inputEl.value = "";
     const parentSection = document.querySelector("#history-info");
+    const existingCity = parentSection.querySelector(`[data-city="${cityName}"]`);
+
+        if (!existingCity) {
     if (parentSection.childNodes.length <13) {
     const asideDiv = document.createElement("section");
     asideDiv.setAttribute('id', "cityName");
-    asideDiv.setAttribute('value', `${cityName}`);
+    asideDiv.setAttribute('data-city', cityName);
     asideDiv.innerHTML = `${cityName}`;
     parentSection.appendChild(asideDiv);
+
+    asideDiv.addEventListener("click", async () => {
+      const coord = await getWeather(cityName); // Передаем cityName в getWeather
+      switchMap(coord);
+  });
     } else {
       const firstSection = document.getElementById("cityName");
       firstSection.remove();
       const asideDiv = document.createElement("section");
       asideDiv.setAttribute('id', "cityName");
-      asideDiv.setAttribute('value', `${cityName}`);
+      asideDiv.setAttribute('data-city', cityName);
       asideDiv.innerHTML = `${cityName}`;
       parentSection.appendChild(asideDiv);
+
+      asideDiv.addEventListener("click", async () => {
+        const coord = await getWeather(cityName); // Передаем cityName в getWeather
+        switchMap(coord);
+    });
     }
-  const weather = await getWeather(cityName);
-    showWeather(weatherInfoEl, weather);
-  });
+  }
+ coord = await getWeather(cityName);
+ switchMap(coord);
+  }); 
+});
+
 })();
